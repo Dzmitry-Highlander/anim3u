ad98e9ee08cbe2d70a6ff9b8e309555bf4167f65
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-15 21:06:02 +0300

    Refactor Anim3uApplication, Anim3uController

--- src/main/java/by/aghanim/anim3u/Anim3uApplication.java
+++ src/main/java/by/aghanim/anim3u/Anim3uApplication.java
@@ -11,7 +11,7 @@ public class Anim3uApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Anim3uApplication.class.getResource("anim3u-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 120);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 135);
 
         stage.setTitle("Anim3u");
         stage.setResizable(false);
