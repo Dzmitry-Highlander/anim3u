ef4111cf41202fb630438312be22c19b635031e3
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-16 18:26:07 +0300

    feat: drag window, refactor: Anim3uApplication

--- src/main/java/by/aghanim/anim3u/Anim3uApplication.java
+++ src/main/java/by/aghanim/anim3u/Anim3uApplication.java
@@ -10,14 +10,19 @@ import java.io.IOException;
 
 public class Anim3uApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Anim3uApplication.class.getResource("new-anim3u-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 400);
 
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        scene.setOnMousePressed(pressEvent -> scene.setOnMouseDragged(dragEvent -> {
+            primaryStage.setX(dragEvent.getScreenX() - pressEvent.getSceneX());
+            primaryStage.setY(dragEvent.getScreenY() - pressEvent.getSceneY());
+        }));
     }
 
     public static void main(String[] args) {
