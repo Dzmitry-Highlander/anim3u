c6d1b79c393960b14fc13552388ad175008cf7be
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-16 19:28:19 +0300

    add: NoSuchTitleException

---
+++ src/main/java/by/aghanim/anim3u/exceptions/NoSuchTitleException.java
@@ -0,0 +1,9 @@
+package by.aghanim.anim3u.exceptions;
+
+import java.io.IOException;
+
+public class NoSuchTitleException extends IOException {
+    public NoSuchTitleException(String message) {
+        super(message);
+    }
+}
